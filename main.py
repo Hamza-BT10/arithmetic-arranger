99385fed9d0f9e04889fbfd3c49d53086484f0b1
HamzaBT10 <oubadi.october@gmail.com>
2025-05-21 16:02:55 +0100

    change variable names

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ class Solution(object):
     @staticmethod
     def arithmetic_arranger(problems: list, show_answers=False):
         split_list =[]
-        first_num = []
-        second_num = []   
+        first_num_list = []
+        second_num_list = []   
         result = []
         def chech_Nproblems(problems):
             return  len(problems)>5
         def Convert_list(problems):
-            for problem in problems:
-                split_list.append(problem.split())
+            for arg in problems:
+                split_list.append(arg.split())
             return split_list
         def check_NLenth(problems):
-            for problem in problems:
-                if len(problem[0]) > 4 or len(problem[-1]) > 4:
+            for arg in problems:
+                if len(arg[0]) > 4 or len(arg[-1]) > 4:
                     print("Error: Numbers cannot be more than four digits.")
                     return False
                 else:
                     continue
             return True
         def Is_Degit(problems):
-            for problem in problems:
-                if problem[0].isdigit() and problem[-1].isdigit():
+            for arg in problems:
+                if arg[0].isdigit() and arg[-1].isdigit():
                     continue
                 else:
                     print('Error: Numbers must only contain digits.')
@@ -45,32 +45,31 @@ class Solution(object):
                 return False
             return True   
         def Get_result(problems):
-            for object in problems:
-                if object[1] == "+":
-                    result.append(str(int(object[0]) + int(object[-1])))
-                elif object[1] == "-":
-                    result.append(str(int(object[0]) - int(object[-1])))           
+            for arg in problems:
+                if arg[1] == "+":
+                    result.append(str(int(arg[0]) + int(arg[-1])))
+                elif arg[1] == "-":
+                    result.append(str(int(arg[0]) - int(arg[-1])))           
             return result
-        def Ajust_Lenth(problems,result: list): 
-            i = 0 
-            for problem in problems:
-                if len(problem[0]) < len(problem[-1]):
-                    len_num =' '* (abs(len(problem[0]) - len(problem[-1]))+2)
-                    problem[0] = len_num+problem[0]
-                    problem[-1] =problem[1]+' '+ problem[-1]
-                    result[i] = ' '*(len(problem[-1])-len(result[i]))+result[i]
-                elif len(problem[0]) > len(problem[-1]):
-                    len_num =' '* (abs(len(problem[0]) - len(problem[-1])))
-                    problem[-1] =problem[1]+len_num + problem[-1]
-                    problem[0] = ' ' +problem[0]
-                    result[i] = ' '*(len(problem[-1])-len(result[i]))+result[i]
+        def Ajust_Lenth(problems,result: list,i = 0 ): 
+            for arg in problems:
+                if len(arg[0]) < len(arg[-1]):
+                    diffe_lenth = ' '* (abs(len(arg[0]) - len(arg[-1]))+2)
+                    arg[0] = diffe_lenth+arg[0]
+                    arg[-1] =arg[1]+' '+ arg[-1]
+                    result[i] = ' '*(len(arg[-1])-len(result[i]))+result[i]
+                elif len(arg[0]) > len(arg[-1]):
+                    diffe_lenth =' '* (abs(len(arg[0]) - len(arg[-1])))
+                    arg[-1] =arg[1]+diffe_lenth + arg[-1]
+                    arg[0] = ' ' +arg[0]
+                    result[i] = ' '*(len(arg[-1])-len(result[i]))+result[i]
                 else:
-                    problem[0] = '  '+problem[0]
-                    problem[-1] = problem[1] + ' '+ problem[-1]
+                    arg[0] = '  '+arg[0]
+                    arg[-1] = arg[1] + ' '+ arg[-1]
                     result[i] = '  '+ result[i]
                 i+=1   
-                first_num.append(problem[0])
-                second_num.append(problem[-1]) 
+                first_num_list.append(arg[0])
+                second_num_list.append(arg[-1]) 
     
         def print_arithmetic_arranger(problems, show_answers=False):
             problems = Convert_list(problems)
@@ -78,14 +77,14 @@ class Solution(object):
                 return
             result = Get_result(problems)
             Ajust_Lenth(problems,result)
-            for i in range(len(first_num)):
-                print(f"{first_num[i]}{' ':>4}",end='')  
+            for i in range(len(first_num_list)):
+                print(f"{first_num_list[i]}{' ':>4}",end='')  
             print()         
-            for i in range(len(second_num)):
-                print(f"{second_num[i]}{' ':>4}",end='')
+            for i in range(len(second_num_list)):
+                print(f"{second_num_list[i]}{' ':>4}",end='')
             print()         
-            for i in range(len(second_num)):
-                print(f"{'-'*len(second_num[i])}{' ':>4}",end='')
+            for i in range(len(second_num_list)):
+                print(f"{'-'*len(second_num_list[i])}{' ':>4}",end='')
             if show_answers:
                 print()
                 for i in range(len(result)):
@@ -100,3 +99,6 @@ class Solution(object):
 problems = ["32 - 698","3801 - 2", "45 + 43", "1239 + 499","225 - 9999"]
 Solution.arithmetic_arranger(problems,show_answers=True)
 Solution.arithmetic_arranger(problems)
+a = "John"
+a =  "{:>15}".format(a)
+print(a,a[0])
